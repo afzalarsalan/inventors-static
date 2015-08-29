78aac14c7d5a132557e83e2788a22ffd30117624
Arsalan Afzal <afzal.arsalan@gmail.com>
2015-08-29 04:02:48 -0500

    Initial commit based of MDL Template

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+console.log('\'Allo \'Allo!');
+
+$(document).ready(function() {
+    //initialize swiper when document ready  
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true
+    })
+});
